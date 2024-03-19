69a4ba9b0b2a5f36d6eeb60aa0f26413e853b52f
Andrei <Andrei@DESKTOP-K91SUSC>
2024-03-19 21:53:26 +0200

    Add project files.

---
+++ Tema1Excerciul5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace Lab1Ex5 
+{ 
+    public class Program
+    /* Ex 5
+Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele
+doua valori in ordine crescatoare.
+• Exemplu: citim 9,0 Afisam : 0 9 
+     */
+    {
+        public static void Main(string[] args)
+        { 
+            int primulNumar;
+            int alDoileaNumar;
+            Console.WriteLine("Introduceti primul numar:");
+            primulNumar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduceti al doilea numar:");
+            alDoileaNumar = Convert.ToInt32(Console.ReadLine());
+            if (primulNumar < alDoileaNumar)
+            { 
+                Console.WriteLine(primulNumar + ", " + alDoileaNumar);
+            } 
+            else 
+            { 
+                Console.WriteLine(alDoileaNumar + ", " + primulNumar);
+            }
+        }
+    }
+}
